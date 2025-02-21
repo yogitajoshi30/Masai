bdd988fdae9bd1b75b8fd59b5fb19cad77a7c500
yogitajoshi30 <joshiyogita67@gmail.com>
2025-02-21 14:04:23 +0530

    custom methods to filter out the even elements from an array, find the sum of an array, and sorting the array

--- customMethods.js
+++ customMethods.js
@@ -10,10 +10,13 @@ function sortAndConcat(arr1, arr2) {
     let sortArr1 = arr1.sort((a, b) => a - b)
     let sortArr2 = arr2.sort((a, b) => a - b)
     let concatenated = sortArr1.concat(sortArr2)
-    return [sortArr1, sortArr2, concatenated];
+    return concatenated;
 }
 let arr1 = [24, 9, 11, 12, 6, 21];
-let arr2 = [4, 2, 6, 8, 54, 12]
+let arr2 = [4, 2, 6, 8, 54, 12];
+
 console.log(filterEvenNumbers(arr1));
+
 console.log(sumOfArray(arr2));
+
 console.log(sortAndConcat(arr1, arr2));
